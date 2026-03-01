8661ad31b2627477014a373bb6a6334c62beafad
Makan KOUYATÉ <kouyatemakan100@gmail.com>
2026-03-01 22:32:49 +0000

    feat: création de l'entité Semestre

---
+++ src/main/java/com/academiq/entity/Semestre.java
@@ -0,0 +1,46 @@
+package com.academiq.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "semestres", uniqueConstraints = @UniqueConstraint(columnNames = {"niveau_id", "semestre"}))
+public class Semestre extends BaseEntity {
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SemestreEnum semestre;
+
+    @Column(nullable = false, length = 100)
+    private String nom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "niveau_id", nullable = false)
+    private Niveau niveau;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "semestre", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UniteEnseignement> uniteEnseignements = new ArrayList<>();
+}
