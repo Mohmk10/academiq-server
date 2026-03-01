1faa3e8e298e09670222b2a0728ca913e14a6079
Makan KOUYATÉ <kouyatemakan100@gmail.com>
2026-03-01 22:32:02 +0000

    feat: création de l'entité Filiere

---
+++ src/main/java/com/academiq/entity/Filiere.java
@@ -0,0 +1,49 @@
+package com.academiq.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "filieres")
+public class Filiere extends BaseEntity {
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String code;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String nom;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private boolean actif = true;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "responsable_id")
+    private Enseignant responsable;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "filiere", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Niveau> niveaux = new ArrayList<>();
+}
